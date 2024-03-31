e767590bdba82b159e37ba0f64c3680831ea41d2
Hakkar <zugrutus@gmail.com>
2024-03-31 23:40:20 +0100

    established base

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD2YzQcTrGU0Ez4y6hLMDTuE2k-iJvnxoc",
+  authDomain: "wetflix-17954.firebaseapp.com",
+  projectId: "wetflix-17954",
+  storageBucket: "wetflix-17954.appspot.com",
+  messagingSenderId: "348046679060",
+  appId: "1:348046679060:web:a0901a2ecb953d3fc19b01",
+};
+
+export default firebaseConfig;
